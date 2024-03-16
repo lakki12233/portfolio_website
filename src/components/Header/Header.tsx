235bdc83c90bc2ac82bd0fdf2ad49f5a332edb81
SUman <sumanm2003@gmail.com>
2024-03-16 22:18:50 +0530

    4th commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
 import  download from '../../assets/download.png';
 
-import CV from '../../assets/Resume_SumanMandava_.pdf' 
+import CV from '../../assets/Resume_SumanMandava_nj.pdf';
 export function Header() {
   const [isActive, setActive] = useState(false)
 
